04e7bc32f2250c97266e129e6255e58062ff0161
Ekaterina <gmees2016@gmail.com>
2021-07-30 18:02:30 +0700

    Add genDiff implementation for flat JSON files

---
+++ src/gendiff.js
@@ -0,0 +1,50 @@
+import { readFileSync } from 'fs';
+import { extname } from 'path';
+import _ from 'lodash';
+
+const readFile = (filePath) => {
+  const content = readFileSync(filePath);
+  return { content, path: filePath };
+};
+
+const parseFile = (file) => {
+  let parsedContent = {};
+  const ext = extname(file.path);
+  if (ext.toLowerCase() === '.json') {
+    parsedContent = JSON.parse(file.content);
+  }
+  return parsedContent;
+};
+
+const compareObjects = (obj1, obj2) => {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  const allKeys = _.sortBy(_.union(keys1, keys2));
+
+  const diffs = allKeys.map((key) => {
+    if (!_.has(obj1, key)) {
+      return `  + ${key}: ${obj2[key]}`;
+    }
+    if (!_.has(obj2, key)) {
+      return `  - ${key}: ${obj1[key]}`;
+    }
+    if (obj1[key] === obj2[key]) {
+      return `    ${key}: ${obj1[key]}`;
+    }
+    return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
+  });
+
+  return ['{', ...diffs, '}'].join('\n');
+};
+
+const genDiff = (filepath1, filepath2) => {
+  const file1 = readFile(filepath1);
+  const file2 = readFile(filepath2);
+
+  const object1 = parseFile(file1);
+  const object2 = parseFile(file2);
+
+  return compareObjects(object1, object2);
+};
+
+export default genDiff;
