e179c08d698d1d2fd63bde467df8f435797ef4f1
Ekaterina <gmees2016@gmail.com>
2021-07-30 18:04:34 +0700

    Add genDiff call in bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
+import genDiff from '../src/gendiff.js';
 
 const program = new Command();
 
@@ -9,6 +10,9 @@ program
   .version('0.0.1')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
