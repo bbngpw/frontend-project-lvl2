5483d958e509b325eed8f54afbb5ad79f6616a15
Ekaterina <gmees2016@gmail.com>
2021-07-30 18:05:35 +0700

    Add testcase for flat JSON files

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,21 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import genDiff from '../src/gendiff.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+
+test('genDiff', () => {
+  const path1 = getFixturePath('file1.json');
+  const path2 = getFixturePath('file2.json');
+  expect(genDiff(path1, path2)).toBe(`{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`);
+});
