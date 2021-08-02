7413a0374bc1e7e9cfdb7d561df9c6d4c4f0fbe3
Ekaterina <gmees2016@gmail.com>
2021-08-02 11:59:58 +0700

    Add checks to test for genDiff with flat json files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,9 +7,10 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 
-test('genDiff', () => {
+test('genDiff with flat json files', () => {
   const path1 = getFixturePath('file1.json');
   const path2 = getFixturePath('file2.json');
+
   expect(genDiff(path1, path2)).toBe(`{
   - follow: false
     host: hexlet.io
@@ -17,5 +18,21 @@ test('genDiff', () => {
   - timeout: 50
   + timeout: 20
   + verbose: true
+}`);
+
+  expect(genDiff(path2, path1)).toBe(`{
+  + follow: false
+    host: hexlet.io
+  + proxy: 123.234.53.22
+  - timeout: 20
+  + timeout: 50
+  - verbose: true
+}`);
+
+  expect(genDiff(path1, path1)).toBe(`{
+    follow: false
+    host: hexlet.io
+    proxy: 123.234.53.22
+    timeout: 50
 }`);
 });
