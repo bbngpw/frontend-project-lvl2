8bfd0371cfd3b42e91681cd65312b4ceb2231eb4
Nika <gmees2016@gmail.com>
2021-09-02 14:51:43 +0700

    Add tests for flat yaml files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -36,3 +36,33 @@ test('genDiff with flat json files', () => {
     timeout: 50
 }`);
 });
+
+test('genDiff with flat yaml files', () => {
+  const path1 = getFixturePath('file1.yml');
+  const path2 = getFixturePath('file2.yml');
+
+  expect(genDiff(path1, path2)).toBe(`{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`);
+
+  expect(genDiff(path2, path1)).toBe(`{
+  + follow: false
+    host: hexlet.io
+  + proxy: 123.234.53.22
+  - timeout: 20
+  + timeout: 50
+  - verbose: true
+}`);
+
+  expect(genDiff(path1, path1)).toBe(`{
+    follow: false
+    host: hexlet.io
+    proxy: 123.234.53.22
+    timeout: 50
+}`);
+});
