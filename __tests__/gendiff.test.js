618fd9f29fffa6b94531fa3200605fa263e22c9c
Nika <gmees2016@gmail.com>
2021-09-02 15:55:37 +0700

    Add tests for nested json and yaml files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,62 +7,246 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 
-test('genDiff with flat json files', () => {
+test('genDiff with nested json files', () => {
   const path1 = getFixturePath('file1.json');
   const path2 = getFixturePath('file2.json');
 
   expect(genDiff(path1, path2)).toBe(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow:
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
 }`);
 
   expect(genDiff(path2, path1)).toBe(`{
-  + follow: false
-    host: hexlet.io
-  + proxy: 123.234.53.22
-  - timeout: 20
-  + timeout: 50
-  - verbose: true
+    common: {
+      - follow: false
+        setting1: Value 1
+      + setting2: 200
+      - setting3: null
+      + setting3: true
+      - setting4: blah blah
+      - setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: so much
+              + wow:
+            }
+            key: value
+          - ops: vops
+        }
+    }
+    group1: {
+      - baz: bars
+      + baz: bas
+        foo: bar
+      - nest: str
+      + nest: {
+            key: value
+        }
+    }
+  + group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  - group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
 }`);
 
   expect(genDiff(path1, path1)).toBe(`{
-    follow: false
-    host: hexlet.io
-    proxy: 123.234.53.22
-    timeout: 50
+    common: {
+        setting1: Value 1
+        setting2: 200
+        setting3: true
+        setting6: {
+            doge: {
+                wow:
+            }
+            key: value
+        }
+    }
+    group1: {
+        baz: bas
+        foo: bar
+        nest: {
+            key: value
+        }
+    }
+    group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
 }`);
 });
 
-test('genDiff with flat yaml files', () => {
+test('genDiff with nested yaml files', () => {
   const path1 = getFixturePath('file1.yml');
   const path2 = getFixturePath('file2.yml');
 
   expect(genDiff(path1, path2)).toBe(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow:
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
 }`);
 
   expect(genDiff(path2, path1)).toBe(`{
-  + follow: false
-    host: hexlet.io
-  + proxy: 123.234.53.22
-  - timeout: 20
-  + timeout: 50
-  - verbose: true
+    common: {
+      - follow: false
+        setting1: Value 1
+      + setting2: 200
+      - setting3: null
+      + setting3: true
+      - setting4: blah blah
+      - setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: so much
+              + wow:
+            }
+            key: value
+          - ops: vops
+        }
+    }
+    group1: {
+      - baz: bars
+      + baz: bas
+        foo: bar
+      - nest: str
+      + nest: {
+            key: value
+        }
+    }
+  + group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  - group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
 }`);
 
   expect(genDiff(path1, path1)).toBe(`{
-    follow: false
-    host: hexlet.io
-    proxy: 123.234.53.22
-    timeout: 50
+    common: {
+        setting1: Value 1
+        setting2: 200
+        setting3: true
+        setting6: {
+            doge: {
+                wow:
+            }
+            key: value
+        }
+    }
+    group1: {
+        baz: bas
+        foo: bar
+        nest: {
+            key: value
+        }
+    }
+    group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
 }`);
 });
