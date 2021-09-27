731be7c56b4b02e6834b36b78c2eb9fd05eadd8c
Nika Perfilova <gmees2016@gmail.com>
2021-09-27 15:38:24 +0700

    Add a default value  fto rmat opconsole tion f

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,7 +10,7 @@ program
   .version('0.0.1')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2, options) => {
     console.log(genDiff(filepath1, filepath2, options.format));
   });
