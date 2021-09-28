a528c785fe673ae184f460564d77d95210cdfefd
Nika Perfilova <gmees2016@gmail.com>
2021-09-28 17:06:30 +0700

    Add implementation for formatter 'plain'

--- src/formatters.js
+++ src/formatters.js
@@ -68,10 +68,46 @@ const formatIter = (nodes, depth = 0) => {
 
 const formatStylish = (diff) => ['{', ...formatIter(diff), '}'].join('\n');
 
+const valueToStr = (value) => {
+  if (_.isString(value)) {
+    return `'${value}'`;
+  }
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+  return String(value);
+};
+
+const formatPlainIter = (nodes, nodesPath) => {
+  const lines = nodes
+    .filter((node) => !(node.status === 'unchanged' && node.children === null))
+    .flatMap((node) => {
+      const newPath = nodesPath ? `${nodesPath}.${node.name}` : node.name;
+      if (node.status === 'unchanged') {
+        return formatPlainIter(node.children, newPath);
+      }
+      const lineStart = `Property '${newPath}' was ${node.status}`;
+      if (node.status === 'removed') {
+        return lineStart;
+      }
+      if (node.status === 'added') {
+        return `${lineStart} with value: ${valueToStr(node.value)}`;
+      }
+      const oldValueStr = valueToStr(node.oldValue);
+      const newValueStr = valueToStr(node.newValue);
+      return `${lineStart}. From ${oldValueStr} to ${newValueStr}`;
+    });
+  return lines;
+};
+
+const formatPlain = (diff) => formatPlainIter(diff).join('\n');
+
 const formatDiff = (diff, format) => {
   switch (format) {
     case 'stylish':
       return formatStylish(diff);
+    case 'plain':
+      return formatPlain(diff);
     default:
       return '';
   }
